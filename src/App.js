1ea3ae132f450e5b479c87307a80b30475c8197e
Talita Beneves <talitabeneves15@gmail.com>
2021-11-13 19:23:37 -0300

    Evoluindo Form

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 let initialState = [
     {
       id: 1,
+      prioridade: 'Normal',
+      titulo: 'título',
       descricao: 'Batata'
     },
     {
       id: 2,
+      prioridade: 'Normal',
+      titulo: 'título',
       descricao: 'poTAto'
     },
 ];
@@ -20,6 +24,8 @@ function App() {
 
     const atividade = {
       id: document.getElementById('id').value,
+      prioridade: document.getElementById('prioridade').value,
+      titulo: document.getElementById('titulo').value,
       descricao: document.getElementById('descricao').value,
     };
 
@@ -28,42 +34,57 @@ function App() {
 
   return (
     <>
-      <form className="row g-3">
-      <div className="col-md-6">
-        <label for="inputEmail4" className="form-label">Id</label>
+      <form className="row g-3 ms-2">
+      <div className="col-md-4">
+        <label className="form-label">Id</label>
         <input  
         id="id" 
         type="text"  
         className="form-control" 
         />
       </div>
-      <div className="col-md-6">
-        <label for="inputEmail4" className="form-label">Descrição</label>
+      <div className="col-md-4">
+        <label className="form-label">Prioridade</label>
+        <select id="prioridade" className="form-select">
+          <option defaultValue="0">Selecione</option>
+          <option value="1">Baixa</option>
+          <option value="2">Normal</option>
+          <option value="3">Alta</option>
+        </select>
+      </div>
+      <div className="col-md-4">
+        <label className="form-label">Título</label>
+        <input  
+        id="titulo" 
+        type="text"  
+        className="form-control" 
+        />
+      </div>
+      <div className="col-md-4">
+        <label className="form-label">Descrição</label>
         <input  
         id="descricao" 
         type="text"  
         className="form-control" 
         />
-        <hr/>
-        <div className="col-12">
-        <button className="btn btn-outline-success" onClick={addAtividade} > ➕ Atividade</button>
-        </div>
       </div>
-      <br/>
       </form>
-      <div className="mt-3">
+      <div className="mt-3 ms-2">
+      <div className="col-12 ms-2 mb-3">
+        <button className="btn btn-outline-success" onClick={addAtividade} ><i class="fas fa-plus-circle"></i> Atividade</button>
+        </div>
             {atividades.map((ativ) => (
-              <div key={ativ.id} className="card mb-2 shadow-sm" style={{width: "20rem"}}>
+              <div key={ativ.id} className="card ms-2 mb-2 shadow-sm" style={{width: "20rem"}}>
                 <div className="card-body">
                   <div className="d-flex justify-content-between">
                     <h5 className="card-title">
                     <span className="badge bg-secondary me-1">{ativ.id}</span>
-                     - título
+                     {ativ.titulo}
                     </h5>
                     <h6>Prioridade: 
                       <span className="ms-1 text-black">
                         <i className="me-1 far fa-smile"></i>
-                          Normal
+                          {ativ.prioridade}
                       </span>
                     </h6>
                   </div>
