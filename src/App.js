3a0d2b6bc6d190f42742792f18ed5b3744645031
Talita Beneves <talitabeneves15@gmail.com>
2021-11-13 22:37:01 -0300

    criação de components e melhora no cod

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import './App.css';
+import  AtividadeForm from './components/AtividadeForm';
+import  Atividade from './components/Atividade';
 
 let initialState = [
     {
@@ -43,108 +45,29 @@ function App() {
     setAtividades([...atividadesFiltradas]);
   }
 
-  function prioridadeLabel(param){
-    switch(param){
-      case '1':
-        return 'Baixa';
-      case '2':
-        return 'Normal';
-      case '3':
-        return 'Alta';
-        default:
-          return 'Não definido';
-    }
-  }
-
-  function prioridadeStyle(param, icon){
-    switch (param) {
-      case '1':
-        return icon ? 'grin-beam' : 'success';
-      case '2':
-        return icon ? 'smile' : 'dark';
-      case '3':
-        return icon ? 'grimace' : 'danger';
-      default:
-          return 'Não definido';
-    };
-  };
-
-
   return (
     <>
-      <form className="row g-3 ms-2">
-      <div className="col-md-4">
-        <label className="form-label">Id</label>
-        <input  
-          id="id" 
-          type="text"  
-          className="form-control"
-          readOnly 
-          value={Math.max.apply(Math, atividades.map(item => item.id)) + 1}
-        />
-      </div>
-      <div className="col-md-4">
-        <label className="form-label">Prioridade</label>
-        <select id="prioridade" className="form-select">
-          <option defaultValue="0">Selecione</option>
-          <option value="1">Baixa</option>
-          <option value="2">Normal</option>
-          <option value="3">Alta</option>
-        </select>
-      </div>
-      <div className="col-md-4">
-        <label className="form-label">Título</label>
-        <input  
-          id="titulo" 
-          type="text"  
-          className="form-control" 
-        />
-      </div>
-      <div className="col-md-4">
-        <label className="form-label">Descrição</label>
-        <input  
-          id="descricao" 
-          type="text"  
-          className="form-control" 
-        />
-      </div>
-      </form>
+      <AtividadeForm 
+        atividades={atividades}
+      />
       <div className="mt-3 ms-2">
-      <div className="col-12 ms-2 mb-3">
-        <button className="btn btn-outline-info" onClick={addAtividade} ><i className="fas fa-plus-circle"></i> ADICIONE</button>
-        </div>
+        <div className="col-12 ms-2 mb-3">
+          <button 
+            className="btn btn-outline-info" 
+            onClick={addAtividade} >
+              <i className="fas fa-plus-circle"> ADICIONE</i> 
+          </button>
+          </div>
+          <div className='mt-3'>
             {atividades.map((ativ) => (
-              <div key={ativ.id} className={'card ms-2 mb-2 shadow-sm border-' + prioridadeStyle(ativ.prioridade)} >
-                <div className="card-body">
-                  <div className="d-flex justify-content-between">
-                    <h5 className="card-title">
-                    <span className="badge bg-secondary me-1">{ativ.id}</span>
-                     {ativ.titulo}
-                    </h5>
-                    <h6>Prioridade: 
-                      <span className={'ms-1 text-' + prioridadeStyle(ativ.prioridade)}>
-                        <i className={'me-1 far fa-' + prioridadeStyle(ativ.prioridade, true)}></i>
-                        {prioridadeLabel(ativ.prioridade)}
-                      </span>
-                    </h6>
-                  </div>
-                  <p className="card-text"> {ativ.descricao} </p>
-                  <div className="d-flex justify-content-end pt-2 m-0 border-top">
-                    <button className="btn-sm btn btn-outline-info me-2 ">
-                      <i className="fas fa-pen me-2"></i>
-                      Editar
-                    </button>
-                    <button 
-                      className="btn-sm btn btn-outline-danger" 
-                      onClick={() => deletarAtividade(ativ.id)}>
-                    <i className="fas fa-trash me-2"></i>
-                      Deletar
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
-      </div>
+              <Atividade  
+                key={ativ.id} 
+                ativ={ativ}
+                deletarAtividade={deletarAtividade}
+              />
+              ))}
+            </div>
+        </div>
    </> 
   );
 }
